638fb48c67528e957dd12e40265ebe22ba5e4167
Oren Roth <oroth@lightricks.com>
2022-07-21 10:38:46 +0300

    change from mock to real API

--- app/adapters/daily-forecast.js
+++ app/adapters/daily-forecast.js
@@ -4,16 +4,16 @@ export default class DailyForecastAdapter extends JSONAPIAdapter {
   /*
   comment all the following lines for mock API
   */
-  // host = '//dataservice.accuweather.com';
-  // namespace = 'forecasts/v1/daily/5day';
-  // query(store, type, query, recordArray, adapterOptions) {
-  //   const params = {
-  //     apikey: encodeURIComponent(ENV.ACCUWEATHER_APIKEY),
-  //   };
-  //   return this.ajax(this.buildURL(query), 'GET', { data: params });
-  // }
-  // //in order to override adding 's' in the end of the modelname
-  // pathForType(modelName) {
-  //   return modelName;
-  // }
+  host = '//dataservice.accuweather.com';
+  namespace = 'forecasts/v1/daily/5day';
+  query(store, type, query, recordArray, adapterOptions) {
+    const params = {
+      apikey: encodeURIComponent(ENV.ACCUWEATHER_APIKEY),
+    };
+    return this.ajax(this.buildURL(query), 'GET', { data: params });
+  }
+  //in order to override adding 's' in the end of the modelname
+  pathForType(modelName) {
+    return modelName;
+  }
 }
