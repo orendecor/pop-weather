f3587fd4ce70a1e4224dc3ddfcf7bb28605072de
Oren Roth <oroth@lightricks.com>
2022-07-21 09:23:57 +0300

    make mock API

--- app/adapters/daily-forecast.js
+++ app/adapters/daily-forecast.js
@@ -1,16 +1,19 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import ENV from '../config/environment';
 export default class DailyForecastAdapter extends JSONAPIAdapter {
-  host = '//dataservice.accuweather.com';
-  namespace = 'forecasts/v1/daily/5day';
-  query(store, type, query, recordArray, adapterOptions) {
-    const params = {
-      apikey: encodeURIComponent(ENV.ACCUWEATHER_APIKEY),
-    };
-    return this.ajax(this.buildURL(query), 'GET', { data: params });
-  }
-  //in order to override adding 's' in the end of the modelname
-  pathForType(modelName) {
-    return modelName;
-  }
+  /*
+  comment all the following lines for mock API
+  */
+  // host = '//dataservice.accuweather.com';
+  // namespace = 'forecasts/v1/daily/5day';
+  // query(store, type, query, recordArray, adapterOptions) {
+  //   const params = {
+  //     apikey: encodeURIComponent(ENV.ACCUWEATHER_APIKEY),
+  //   };
+  //   return this.ajax(this.buildURL(query), 'GET', { data: params });
+  // }
+  // //in order to override adding 's' in the end of the modelname
+  // pathForType(modelName) {
+  //   return modelName;
+  // }
 }
